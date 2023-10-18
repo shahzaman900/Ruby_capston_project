1f589f32cf9055df1016b9f1a1784d80ce5c9b09
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-10-18 21:25:57 +0100

    Correct Linters

--- label.rb
+++ label.rb
@@ -1,16 +1,16 @@
 class Label
-    attr_accessor :name, :color
-    attr_reader :id, :items
-  
-    def initialize(id, title, color)
-      @id = id || Random.rand(1..1000)  
-      @name = name
-      @color = color
-      @items = []
-    end
-  
-    def add_item(item)
-      @items << item
-      item.label = self
-    end
+  attr_accessor :name, :color
+  attr_reader :id, :items
+
+  def initialize(id, _title, color)
+    @id = id || Random.rand(1..1000)
+    @name = name
+    @color = color
+    @items = []
   end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+end
