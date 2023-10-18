187243dc35e19a241e8a1849ee13852f56ea9d19
akorede <emiolasaheed1@gmail.com>
2023-10-18 15:33:01 +0100

    Create custom setter method for properties for the 1-t-many relationships

--- item.rb
+++ item.rb
@@ -1,9 +1,29 @@
 class Item
-  attr_accessor :title, :published_date, :item_owner
+  attr_accessor :genre, :author, :source, :label, :published_date
 
   def initialize(publish_date)
     @id = rand(1..100)
     @publish_date = publish_date
     @archived = false
+  end
+
+  def genre=(genre)
+    genre.items << self unless genre.items.include?(self)
+    @genre = genre
+  end
+
+  def author=(author)
+    author.items << self unless author.items.include?(self)
+    @author = author
+  end
+
+  def source=(source)
+    source.items << self unless source.items.include?(self)
+    @source = source
+  end
+
+  def label=(label)
+    label.items << self unless label.items.include?(self)
+    @label = label
   end
 end
