8f352acae273c2fb923f62f4826fafeb1c00b029
akorede <emiolasaheed1@gmail.com>
2023-10-18 15:23:21 +0100

    Create item class constructor

--- item.rb
+++ item.rb
@@ -1,3 +1,9 @@
 class Item
   attr_accessor :title, :published_date, :item_owner
+
+  def initialize(publish_date)
+    @id = rand(1..100)
+    @publish_date = publish_date
+    @archived = false
+  end
 end
