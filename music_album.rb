88ec7db330f54fce774f43abfc5e08a59c54ec40
akorede <emiolasaheed1@gmail.com>
2023-10-19 10:56:25 +0100

    Create music album class with its methods

---
+++ music_album.rb
@@ -0,0 +1,15 @@
+require_relative 'item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(published_date, on_spotify)
+    super(published_date)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && on_spotify
+  end
+end
+
