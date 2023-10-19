8ef4d184edb213a110cd54148529a9b6a4c4668b
akorede <emiolasaheed1@gmail.com>
2023-10-19 13:52:23 +0100

    Save music album and genres in json format

--- save_music_album.rb
+++ save_music_album.rb
@@ -1,9 +1,11 @@
+require 'json'
+
 class SaveMusic
   def save_music_album(music_albums)
     music_json = music_albums.map do |music|
       { 'publish_date' => music.publish_date, 'on_spotify' => music.on_spotify}
     end
-    File.write(music_album.json, music_json);
+    File.write(music_album.json, JSON.pretty_generate(music_json));
   end
 
   def save_genre(genres)
