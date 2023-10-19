33649b365c55949620412fff092abc63024d7708
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-10-19 10:17:10 +0100

    Finish test for book.rb

--- book.rb
+++ book.rb
@@ -3,15 +3,13 @@ require_relative 'item'
 class Book < Item
   attr_accessor :title, :author, :genre, :cover_state
 
-  def initialize(published_date, title, author, genre, cover_state)
-    super(published_date)
+  def initialize(publish_date, title, author, genre, cover_state)
+    super(publish_date)
     @title = title
     @author = author
     @genre = genre
     @cover_state = cover_state
   end
-
-  private
 
   def can_be_archived?
     super || @cover_state == 'bad'
