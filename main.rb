7320e0ac21063f412354675deed9cec92409edf1
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-10-20 18:13:01 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -32,10 +32,13 @@ class Main
     options = {
       '1' => -> { ListBooks.new.list_books(@app.books) },
       '2' => -> { ListMusicAlbums.new.list_music_album(@app.music_albums) },
+      '3' => -> {  },
       '4' => -> { ListMusicAlbums.new.list_genres(@app.genres) },
       '5' => -> { ListLabels.new.list_labels(@app.label) },
+      '6' => -> {  },
       '7' => -> { BookApp.new.add_book(@app.books) },
       '8' => -> { MusicAlbumApp.new.create_music_album(@app.music_albums, @app.genres) },
+      '9' => -> {  },
       '10' => -> { exit_app }
     }
 
