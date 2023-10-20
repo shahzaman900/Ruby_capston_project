284bddaef93d059f55ff8c9b6edb3d7cfbb15032
akorede <emiolasaheed1@gmail.com>
2023-10-20 17:16:51 +0100

    Write test for genre methods

---
+++ spec/genre_spec.rb
@@ -0,0 +1,21 @@
+
+require 'rspec'
+require './genre'
+require './music_album'
+
+describe Genre do
+  subject { Genre.new('pop') }
+
+  context '#initialize' do
+    it 'initializes properly' do
+      expect(subject.name).to eq('pop')
+    end
+  end
+
+  context '#add_item' do
+    it 'adds items' do
+      music_album = MusicAlbum.new('2023-04-05', true)
+      subject.add_item(item: music_album)
+      expect(music_album.genre).to eq(subject)
+    end
+  end
